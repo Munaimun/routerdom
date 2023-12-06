52564322716c89f1208ede1ab5a3adcda646b35e
Munaimun <munaimunz@gmail.com>
2023-12-06 16:47:42 +0600

    react routers outlet and link connected

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Root from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/contact";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
